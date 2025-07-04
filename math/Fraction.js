3075a1a866b45c61bee1212cd3fb99be8e5818b4
Barış ER <erbaris@yahoo.com>
2025-07-04 14:47:25 +0300

    Update

--- math/Fraction.js
+++ math/Fraction.js
@@ -1,46 +1,83 @@
 /*
 Sınıf Çalışması: Bir kesri temsil eden Fraction isimli sınıfı aşağıdaki açıklamalara göre yazınız
 Açıklamalar:
-•	Sınıf Matematikteki bir kesri temsil ettiğinden pay (numerator) ve payda (denominator) değerleri tutulacaktır.
-•	Sınıfın ilgili set ve get property elemanları yazılacaktır.
++•	Sınıf Matematikteki bir kesri temsil ettiğinden pay (numerator) ve payda (denominator) değerleri tutulacaktır.
++•	Sınıfın ilgili set ve get property elemanları yazılacaktır.
 •	Pay'ın sıfırdan farklı veya sıfır VE paydanın sıfır olması durumunda uygun mesajlar ile error fırlatılacaktır
-•	Kesir her durumda sadeleşmiş bir biçimde tutulacaktır. Örneğin kesrin pay ve paydası sırasıyla 4 ve 18 olarak verildiğinde kesir 2 / 9 olarak tutulacaktır.
-•	Kesir negatif ise işaret payda bulunacaktır. Örneğin kesrin pay ve paydası sırasıyla 3 ve -4 olarak verilmişse kesir -3 / 4 biçiminde tutulacaktır.
-•	Kesrin pay ve paydasının her ikisinin birden negatif olması durumunda kesir pozitif olarak tutulacaktır.
-•	Kesrin payının sıfır olması durumunda payda ne olursa olsun 1(bir) yapılacaktır.
++•	Kesir her durumda sadeleşmiş bir biçimde tutulacaktır. Örneğin kesrin pay ve paydası sırasıyla 4 ve 18 olarak verildiğinde kesir 2 / 9 olarak tutulacaktır.
++•	Kesir negatif ise işaret payda bulunacaktır. Örneğin kesrin pay ve paydası sırasıyla 3 ve -4 olarak verilmişse kesir -3 / 4 biçiminde tutulacaktır.
++•	Kesrin pay ve paydasının her ikisinin birden negatif olması durumunda kesir pozitif olarak tutulacaktır.
++•	Kesrin payının sıfır olması durumunda payda ne olursa olsun 1(bir) yapılacaktır.
 •	Sınıfın iki kesri toplayan, bir kesir ile bir tamsayıyı toplayan metotları olacaktır. Aynı işlemler çıkarma, çarpma ve bölme için de yapılacaktır.
-•	Sınıfın kesri 1(bir) artıran ve bir azaltan inc ve dec metotları yazılacaktır.
++•	Sınıfın kesri 1(bir) artıran ve bir azaltan inc ve dec metotları yazılacaktır.
 •	Sınıfın toString metodu şu formatta yazı döndürecek şekilde override edilecektir. Örneğin 3 / 10 kesri için -> 3 / 10 = 3.333333 10 / 1 kesri için -> 10 Ondalık kısımda 6 basamak gösterilecektir. Geri kalan basamaklar yuvarlanacaktır.
 •	Sınıfın equals metodu iki kesrin eşitlik karşılaştırması için yazılacaktır.
 •	Sınıfın compareTo metodu iki kesrin büyüklük küçüklük karşılaştırmasını yapacaktır. String sınıfının compareTo metodunun mantığına göre tasarlayınız.
-•	Kesrin double türden ondalık değerini döndüren realValue property elemanı yazılacaktır.
++•	Kesrin double türden ondalık değerini döndüren realValue property elemanı yazılacaktır.
 
 
  */
 
+const gcd = (a, b) => {
+    let min = Math.abs(a) < Math.abs(b) ? Math.abs(a) : Math.abs(b)
+    for(let i = min; min > 0; min--)
+        if(a % i === 0 && b % i === 0)
+            return i
+}
+
+const simplify = (numerator, denominator) => {
+    let divider
+    while((divider = gcd(numerator, denominator)) !== 1) {
+        numerator = numerator / divider
+        denominator = denominator / divider
+    }
+    return [numerator, denominator]
+}
+
 export class Fraction {
     constructor(numerator, denominator) {
-
+        this._numerator = numerator;
+        this._denominator = denominator;
+        if(this._denominator < 0) {
+            this._denominator *= -1
+            this._numerator *= -1
+        }
+        if(this._numerator === 0)
+            this._denominator = 1
+        else
+            [this._numerator, this._denominator] = simplify(this._numerator, this._denominator)
+        this._realValue = this._numerator / this._denominator
     }
 
     set numerator(value) {
-
+        this._numerator = value
+        if(this._numerator === 0)
+            this._denominator = 1
+        else
+            [this._numerator, this._denominator] = simplify(this._numerator, this._denominator)
+        this._realValue = this._numerator / this._denominator
     }
 
     get numerator() {
-
+        return this._numerator
     }
 
     set denominator(value) {
-
+        this._denominator = value
+        if(this._denominator < 0) {
+            this._denominator *= -1
+            this._numerator *= -1
+        }
+        [this._numerator, this._denominator] = simplify(this._numerator, this._denominator)
+        this._realValue = this._numerator / this._denominator
     }
 
     get denominator() {
-
+        return this._denominator
     }
 
     get realValue() {
-
+        return this._realValue
     }
 
     compareTo(other) {
@@ -52,11 +89,11 @@ export class Fraction {
     }
 
     inc() {
-
+        this._numerator += 1
     }
 
     dec() {
-
+        this._numerator -= 1
     }
 
     add(other) {
--- sample.js
+++ sample.js
@@ -1,12 +1,14 @@
-import {write, writeLine} from "./util/console.js"
-import * as numberUtil from "./util/numberUtil.mjs"
-import * as arrayUtil from "./util/arrayUtil.mjs"
-import * as stringUtil from "./util/stringUtil.mjs"
-import {Complex} from "./math/Complex";
+//import {write, writeLine} from "./util/console.js"
+//import * as numberUtil from "./util/numberUtil.mjs"
+//import * as arrayUtil from "./util/arrayUtil.mjs"
+//import * as stringUtil from "./util/stringUtil.mjs"
+//import {Complex} from "./math/Complex.js";
 
 let main = () => {
-    let a = new Complex(3, -5)
-    writeLine(a)
+    //let a = new Complex(3, -5)
+    //writeLine(a)
+
+
 }
 
 main()
