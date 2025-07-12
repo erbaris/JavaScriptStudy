6c8dbcad022ee10caf75a73fa14c65883ba623e8
erbaris <erbaris@yahoo.com>
2025-07-12 16:40:30 +0300

    Update

--- math/Fraction.js
+++ math/Fraction.js
@@ -3,7 +3,7 @@ Sınıf Çalışması: Bir kesri temsil eden Fraction isimli sınıfı aşağıd
 Açıklamalar:
 +•	Sınıf Matematikteki bir kesri temsil ettiğinden pay (numerator) ve payda (denominator) değerleri tutulacaktır.
 +•	Sınıfın ilgili set ve get property elemanları yazılacaktır.
-•	Pay'ın sıfırdan farklı veya sıfır VE paydanın sıfır olması durumunda uygun mesajlar ile error fırlatılacaktır
++•	Pay'ın sıfırdan farklı veya sıfır VE paydanın sıfır olması durumunda uygun mesajlar ile error fırlatılacaktır
 +•	Kesir her durumda sadeleşmiş bir biçimde tutulacaktır. Örneğin kesrin pay ve paydası sırasıyla 4 ve 18 olarak verildiğinde kesir 2 / 9 olarak tutulacaktır.
 +•	Kesir negatif ise işaret payda bulunacaktır. Örneğin kesrin pay ve paydası sırasıyla 3 ve -4 olarak verilmişse kesir -3 / 4 biçiminde tutulacaktır.
 +•	Kesrin pay ve paydasının her ikisinin birden negatif olması durumunda kesir pozitif olarak tutulacaktır.
